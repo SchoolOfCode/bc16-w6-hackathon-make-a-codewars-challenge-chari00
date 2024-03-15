c2226143ea189147baa61a95f2f884ddc3f573d7
chari00 <macharitocruzuk@gmail.com>
2024-03-15 12:15:53 +0000

    test the timer function down correctly.

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+Decrement timer
+
+Hello challenger!
+Your task is to write a function that acts as a countdown timer. 
+It should decrease by one every second. 
+
+This timer will start from a user-provided integer and stop when it reaches zero.
+Only positive numbers are accepted.
+*/
+
+export function decrementTimer(seconds) {
+ 
+
+ return ;
+}
